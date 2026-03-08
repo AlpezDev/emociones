1360990a952065ebd2a4359d35ba6a4007f4dabe
Alpez Developer <alpezdev@gmail.com>
2026-03-08 16:09:51 -0500

    Add files via upload

---
+++ emociones/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def detect_emotion():
+
+    text = request.args.get('textToAnalyze')
+
+    result = emotion_detector(text)
+
+    return str(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+@app.route("/emotionDetector")
+def detect_emotion():
+
+    text = request.args.get('textToAnalyze')
+
+    if text == "":
+        return "Invalid text! Please try again!"
+
+    result = emotion_detector(text)
+
+    return str(result)
